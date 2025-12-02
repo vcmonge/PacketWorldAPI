d6e5b5e99db3a6ec87f754cad653a5fbc5831201
vcmonge <zs22016120@estudiantes.uv.mx>
2025-12-01 20:04:06 -0600

    Inicialización del proyecto

---
+++ src/java/ws/PruebaWS.java
@@ -0,0 +1,33 @@
+
+package ws;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+@Path("generic")
+public class PruebaWS {
+
+    @Context
+    private UriInfo context;
+
+    public PruebaWS() {
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
