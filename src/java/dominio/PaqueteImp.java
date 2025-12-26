dc7f70103d5fca73cf82f4041d507ea85b12a5b6
vcmonge <zs22016120@estudiantes.uv.mx>
2025-12-26 11:41:26 -0600

    fix(PaqueteWS, PaqueteImp): Endpoint retorna List<Paquetes>

--- src/java/dominio/PaqueteImp.java
+++ src/java/dominio/PaqueteImp.java
@@ -1,41 +1,30 @@
 
 package dominio;
 
-import dto.RSPaquete;
 import java.util.List;
 import modelo.mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import pojo.Paquete;
-import utilidades.Constantes;
 
 
 public class PaqueteImp {
     
-    public static RSPaquete obtenerPaquetesPedido(int idEnvio){
-        RSPaquete respuesta = new RSPaquete();
-        respuesta.setError(true);
+    public static List<Paquete> obtenerPaquetesPedido(int idEnvio){
+        List<Paquete> paquetes = null;
+
         SqlSession conexionBD = MyBatisUtil.getSession();
         
         if (conexionBD != null) {
             try {
-                List<Paquete> paquetes = conexionBD.selectList("paquete.obtener-paquetes-envio", idEnvio);
-                if (paquetes.isEmpty()){
-                    respuesta.setMensaje("El envio no tiene paquetes.");
-                } else {
-                    respuesta.setError(false);
-                    respuesta.setMensaje(paquetes.size() + " paquetes obtenidos.");
-                }
-                respuesta.setPaquetes(paquetes);
+                 paquetes = conexionBD.selectList("paquete.obtener-paquetes-envio", idEnvio);
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 conexionBD.close();
             }
-        } else {
-            respuesta.setMensaje(Constantes.MSJ_ERROR_BD);
         }
         
-        return respuesta;
+        return paquetes;
     }
     
 }
--- src/java/ws/PaqueteWS.java
+++ src/java/ws/PaqueteWS.java
@@ -2,13 +2,14 @@
 package ws;
 
 import dominio.PaqueteImp;
-import dto.RSPaquete;
+import java.util.List;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import pojo.Paquete;
 import utilidades.Validaciones;
 
 @Path("paquete")
@@ -17,7 +18,7 @@ public class PaqueteWS {
     @Path("obtener-paquetes-envio/{idEnvio}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public RSPaquete obtenerPaquetesPedido(@PathParam("idEnvio") String idEnvio){
+    public List<Paquete> obtenerPaquetesPedido(@PathParam("idEnvio") String idEnvio){
         if (Validaciones.esVacio(idEnvio)){
             throw new BadRequestException();
         }
