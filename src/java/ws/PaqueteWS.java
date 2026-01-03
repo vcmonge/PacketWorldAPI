1dfb8018bea9a06a656ac6975d0cccadbe408891
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-01-03 11:31:23 -0600

    refactor (obtenerPaquetesPedido): agregar compatibilidad con caracteres especiales

--- src/java/ws/PaqueteWS.java
+++ src/java/ws/PaqueteWS.java
@@ -17,7 +17,7 @@ public class PaqueteWS {
     
     @Path("obtener-paquetes-envio/{idEnvio}")
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public List<Paquete> obtenerPaquetesPedido(@PathParam("idEnvio") String idEnvio){
         if (Validaciones.esVacio(idEnvio)){
             throw new BadRequestException();
